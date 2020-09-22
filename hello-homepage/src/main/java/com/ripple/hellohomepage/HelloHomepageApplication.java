746853eb8ee2475e0ef831d20845189cadb88002
Yuanheng Wang <503342634@qq.com>
2020-09-22 23:34:00 +0800

    add spring project
    init project

---
+++ hello-homepage/src/main/java/com/ripple/hellohomepage/HelloHomepageApplication.java
@@ -0,0 +1,13 @@
+package com.ripple.hellohomepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloHomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloHomepageApplication.class, args);
+	}
+
+}
---
+++ hello-homepage/src/test/java/com/ripple/hellohomepage/HelloHomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ripple.hellohomepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloHomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
