f501eb9a8429a91b0d5a390e82fa048ce663c9ab
Yuanheng Wang <503342634@qq.com>
2020-09-24 19:51:44 +0800

    edit 1

--- hello-homepage/src/main/java/com/ripple/hellohomepage/controller/HelloController.java
+++ hello-homepage/src/main/java/com/ripple/hellohomepage/controller/HelloController.java
@@ -1,13 +1,24 @@
 package com.ripple.hellohomepage.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@ConfigurationProperties(prefix="upload")
 public class HelloController {
-    @GetMapping
+    private String imagePath;
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+    public String getImagePath(){
+        return this.imagePath;
+    }
+    @GetMapping("/1")
     public String root(){
-        return "heihei";
+        return imagePath+"OK";
     }
 }
