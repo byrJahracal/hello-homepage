37a952256fa534f8fcb25c72da2f28227763373a
wangyuanheng <503342634@qq.com>
2020-09-27 23:52:35 +0800

    添加了html界面

--- hello-homepage/src/main/java/com/ripple/hellohomepage/controller/HelloController.java
+++ hello-homepage/src/main/java/com/ripple/hellohomepage/controller/HelloController.java
@@ -1,13 +1,10 @@
 package com.ripple.hellohomepage.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @ConfigurationProperties(prefix="upload")
 public class HelloController {
     private String imagePath;
@@ -17,8 +14,8 @@ public class HelloController {
     public String getImagePath(){
         return this.imagePath;
     }
-    @GetMapping("/1")
+    @GetMapping("/")
     public String root(){
-        return imagePath+"OK";
+        return "hello";
     }
 }
